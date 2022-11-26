21adbc40ff58f2cadcc7cc51a0987b6e8983f4ed
Anurag Shukla <71091279+theanuragshukla@users.noreply.github.com>
2022-11-26 14:01:35 +0000

    Sync LeetCode submission - Sign of the Product of an Array (java)

---
+++ leetcode/problems/sign_of_the_product_of_an_array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for(int x : nums){
+            if(x==0)
+                return 0;
+            if(x<0){
+                count++;
+            }
+        }
+        return count%2==0 ? 1 :-1;
+    }
+}
