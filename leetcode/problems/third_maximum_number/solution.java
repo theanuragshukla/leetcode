8221d58901713a91be438232399958136754e75f
Anurag Shukla <71091279+theanuragshukla@users.noreply.github.com>
2023-01-21 07:28:23 +0000

    Sync LeetCode submission - Third Maximum Number (java)

---
+++ leetcode/problems/third_maximum_number/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+
+        List<Integer> list=new ArrayList<>(set);
+        Collections.sort(list);
+        if(list.size()<3){
+            return list.get(list.size()-1);
+        }
+            return list.get(list.size()-3);
+    }
+}
